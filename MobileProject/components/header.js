74fba178129aa1dbefefd8561ca333833adc7824
sakkinenmiro <127876519+sakkinenmiro@users.noreply.github.com>
2024-11-11 18:42:13 +0200

    draweritems ja header
    draweritems constant johon screenit mihin navigoida headerin hampurilaisvalikolla

---
+++ MobileProject/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import styles from '../style/style';
+
+export default function Header() {
+    return(
+        <View style={styles.header}>
+            
+    
+        </View>
+    )
+}
