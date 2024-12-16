6fd2784abbb976e7c10240d7389f6ac0252856c1
AnPeso <128254858+AnPeso@users.noreply.github.com>
2024-12-16 21:37:50 +0200

    toimiva linkki

--- MobileProject/components/tietoja.js
+++ MobileProject/components/tietoja.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Linking } from 'react-native';
 import style from '../style/style';
-import { ExternalLink } from 'react-external-link';
 
 const Tietoja = ({ navigation }) => {
+  const openLink = (url) => {
+    Linking.openURL(url).catch((err) => console.error('URL:n avaaminen epäonnistui:', err));
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text style={style.title}>Tietoja</Text>
       <Text style={style.text}>Tekijät:</Text>
       <Text style={style.text}>Anni Pesonen, Miro Säkkinen, Frans Puranen</Text>
       <Text style={style.text}>Lähteet:</Text>
-      <Text style={style.text}><ExternalLink href="https://salpakierto.fi/neuvonta/jatehaku/"/></Text>
+
+      {/* Klikattava linkki */}
+      <Text 
+        style={[style.text, { color: 'blue', textDecorationLine: 'underline' }]} 
+        onPress={() => openLink('https://salpakierto.fi/neuvonta/jatehaku/')}
+      >
+        https://salpakierto.fi/neuvonta/jatehaku/
+      </Text>
+
       <TouchableOpacity onPress={() => navigation.goBack()} style={style.button}>
         <Text style={style.buttonText}>Takaisin</Text>
       </TouchableOpacity>
