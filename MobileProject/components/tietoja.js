377cefc392232393cc765a83c2d25bc348186bc5
Fpu001 <127602677+Fpu001@users.noreply.github.com>
2024-12-16 18:34:51 +0200

    Tietoja sivun napin muokkaus

--- MobileProject/components/tietoja.js
+++ MobileProject/components/tietoja.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import style from '../style/style';
 import { ExternalLink } from 'react-external-link';
 
@@ -11,10 +11,9 @@ const Tietoja = ({ navigation }) => {
       <Text style={style.text}>Anni Pesonen, Miro Säkkinen, Frans Puranen</Text>
       <Text style={style.text}>Lähteet:</Text>
       <Text style={style.text}><ExternalLink href="https://salpakierto.fi/neuvonta/jatehaku/"/></Text>
-      <Button 
-        title="Takaisin" 
-        onPress={() => navigation.goBack()} 
-      />
+      <TouchableOpacity onPress={() => navigation.goBack()} style={style.button}>
+        <Text style={style.buttonText}>Takaisin</Text>
+      </TouchableOpacity>
     </View>
   );
 };
